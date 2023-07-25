59b61b89480cda874320ed4d39e4193efde40519
Victor Ajibade <adesojimav@gmail.com>
2023-07-25 11:08:25 +0100

    implemeting jwt

--- Extension/ConfigureServices.cs
+++ Extension/ConfigureServices.cs
@@ -1,7 +1,12 @@
 ﻿using ContactBookAPP.CloudinaryDetails;
 using ContactBookAPP.Context;
+using ContactBookAPP.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace ContactBookAPP.Extension
 {
@@ -14,6 +19,25 @@ namespace ContactBookAPP.Extension
 
 			services.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
 			services.AddScoped<CloudinaryService>();
+
+			services.AddIdentity<Persons, Role>()
+				.AddEntityFrameworkStores<UserDbContext>().AddDefaultTokenProviders();
+
+
+			// Configure JWT authentication
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+		.AddJwtBearer(options =>
+		{
+			options.TokenValidationParameters = new TokenValidationParameters
+			{
+				ValidateIssuerSigningKey = true,
+				IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("YourJwtSecretKey")),
+				ValidateIssuer = false,
+				ValidateAudience = false
+			};
+		});
 		}
+
+
 	}
 }
